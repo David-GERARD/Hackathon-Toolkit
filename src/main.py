8ad2fe897c8a43a62adf77cf483bb1844c819af1
David GERARD <david.gerard.23@ucl.ac.uk>
2024-11-15 12:46:36 +0000

    feat: create main.py

---
+++ src/main.py
@@ -0,0 +1,19 @@
+# Use this file to integrate all the modules you develop in parallel. 
+# Best practice is to import code from other python files and wrap them with functions.
+
+def module1():
+  """
+  Use this function to wrap classes or functions defined in other files
+  """
+  pass
+
+def module2():
+  """
+  Use this function to wrap classes or functions defined in other files
+  """
+  pass
+
+if __name__ == "__main__":
+  # Put your pipeline here
+  module1()
+  module2()
